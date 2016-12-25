4716db973ec2811ef5baf44a6cecf29081e96383
Sanjay <skpatel20@gmail.com>
2016-12-25 11:42:24 +0530

    First commit

---
+++ np-demo-spring-mvc-interceptor/src/main/java/com/naturalprogrammer/spring/mvcinterceptor/NpDemoSpringMvcInterceptorApplication.java
@@ -0,0 +1,12 @@
+package com.naturalprogrammer.spring.mvcinterceptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NpDemoSpringMvcInterceptorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NpDemoSpringMvcInterceptorApplication.class, args);
+	}
+}
