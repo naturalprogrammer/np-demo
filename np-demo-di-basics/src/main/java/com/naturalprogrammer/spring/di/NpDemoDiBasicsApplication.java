bc53e81e83f2b03b9780fbb2b95c2faa9efb7ff7
Sanjay Patel <skpatel20@gmail.com>
2017-01-01 13:54:40 +0530

    First commit of DI Basics

---
+++ np-demo-di-basics/src/main/java/com/naturalprogrammer/spring/di/NpDemoDiBasicsApplication.java
@@ -0,0 +1,12 @@
+package com.naturalprogrammer.spring.di;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NpDemoDiBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NpDemoDiBasicsApplication.class, args);
+	}
+}
